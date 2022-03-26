7a1ae5ebbb0d7a33af89b9cd83cf40daa0b95c7a
(Aiden Tracy) <42971511+PointlessVenture@users.noreply.github.com>
2022-03-26 13:15:27 -0400

    Add files via upload

---
+++ Singulizer.py
@@ -0,0 +1,14 @@
+writefile = open("FullDataset.txt", "w")
+baseFilePath = "Compliments/compliment"
+baseFileSuffix = ".txt"
+totalFiles = 541209
+
+for i in range(1,totalFiles):
+    if(i % 1000 == 0):
+        print(i)
+    file = baseFilePath + str(i) + baseFileSuffix
+    readfile = open(file, "r")
+    text = readfile.read()
+    readfile.close()
+    text += "\n"
+    writefile.write(text)
