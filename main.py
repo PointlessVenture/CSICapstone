5643185c1b79bcfda6f00a130a90e78264bf2461
collin-westgate <49370826+collin-westgate@users.noreply.github.com>
2022-04-04 17:23:18 -0400

    Local-variant upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+from aitextgen_gpt_neo_implementation import generation
+
+if __name__ == '__main__':
+    #run_setup();
+    generation();
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
